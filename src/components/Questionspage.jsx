736326f1d95bcc400a731f6f3f5eb5583484cf44
winfredo <winfrednukpe2002@gmail.com>
2025-08-16 15:00:47 +0000

    fetched data from the opentdb api

---
+++ src/components/Questionspage.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+
+const Questionspage = () => {
+  const [questions, setQuestions] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  function getAnswers(question) {
+    const answers = [...question.incorrect_answers, question.correct_answer];
+    return answers.sort(() => Math.random() - 0.5);
+  }
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
+        );
+        if (!res.ok) {
+          throw new Error("Failed to fetch questions");
+        }
+        const data = await res.json();
+        console.log(data);
+        setQuestions(data.results);
+      } catch (err) {
+        setError(err.message);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>Loading. Please wait..</p>}
+      {error && <p>Error: {error}</p>}
+      <div>
+        {questions.map((question, index) => (
+          <div key={index} className="questions-container">
+            <p>{question.question}</p>
+            <div className="answers-container">
+              {getAnswers(question).map((answer, answerIndex) => (
+                <p className="answer" key={answerIndex}>
+                  <button>{answer}</button>
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      {questions.length > 0 && !loading && (
+        <button className="check-answers-button">Check Answers</button>
+      )}
+    </div>
+  );
+};
+
+export default Questionspage;
