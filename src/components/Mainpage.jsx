d3fc764e96c3cfda254679e930dea601ff5616ee
winfredo <winfrednukpe2002@gmail.com>
2025-10-24 23:16:42 +0000

    fixes

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -4,7 +4,7 @@ const Mainpage = (props) => {
   return (
     <div className='mainpage'>
       <h1 className='title'>Quizzical</h1>
-      <p className='description'>Some description if needed</p>
+      <p className='description'>A simple quiz game.</p>
       <button onClick={props.onClick} className='start-quiz-button'>Start Quiz</button>
     </div>
   )
